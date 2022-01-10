999d4935e2733cbee108b3767b9810b4be913902
Thanh Nguyen <puffle.nnt@gmail.com>
2022-01-10 09:17:05 +0700

    client setup and navbar

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss';
+import AppNavbar from './components/AppNavbar';
+
+function App() {
+  return (
+    <AppNavbar />
+  );
+}
+
+export default App;
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { Container, Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap'
+
+const AppNavbar = () => {
+    const [isOpen, setOpen] = useState(false);
+
+    const toggle = () => {
+        setOpen(!isOpen);
+    }
+
+    return (
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <NavbarBrand href='/'>ShoppingList</NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse navbar isOpen={isOpen}>
+                        <Nav className='ms-auto' navbar>
+                            <NavItem>
+                                <NavLink href='https://github.com/entee28'>Github</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default AppNavbar
