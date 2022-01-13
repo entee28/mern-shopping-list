34a41e1901290da2b49ad8f093c0a40da30487b8
Thanh Nguyen <puffle.nnt@gmail.com>
2022-01-13 10:20:02 +0700

    added auth and error state

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,14 @@ import ShoppingList from './components/ShoppingList';
 import { Provider } from 'react-redux'
 import store from './store'
 import ItemModal from './components/ItemModal';
+import { loadUser } from './actions/authActions';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, [])
+
   return (
     <Provider store={store}>
       <AppNavbar />
--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,8 @@ function auth(req, res, next) {
     const token = req.header('x-auth-token');
 
     // Check for token
-    if (!token) res.status(401).json({ msg: 'No token, authorization denied' });
+    if (!token)
+        return res.status(401).json({ msg: 'No token, authorization denied' });
 
     try {
         // Verify token
@@ -14,7 +15,7 @@ function auth(req, res, next) {
         req.user = decoded;
         next();
     } catch (e) {
-        res.status(400).json({msg: 'Token is not valid'})
+        res.status(400).json({ msg: 'Token is not valid' })
     }
 }
 
